1c73173829ca4476750f22b8d70cd8af49f6869b
Rahul Ramjeawon <96823012+CodeWithTejpratab@users.noreply.github.com>
2024-12-04 06:40:08 -0500

    phase 2
    * save button now update time

--- app/src/main/java/com/example/recordkeeper/EditCyclingRecordActivity.kt
+++ app/src/main/java/com/example/recordkeeper/EditCyclingRecordActivity.kt
@@ -5,17 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.recordkeeper.databinding.ActivityEditCyclingRecordBinding
+import com.example.recordkeeper.roomDataBase.RecordDao
+import com.example.recordkeeper.roomDataBase.RecordLogDatabase
+import com.example.recordkeeper.roomDataBase.RecordType
+import com.example.recordkeeper.roomDataBase.getCurrentFormattedDate
+import kotlinx.coroutines.launch
 
 class EditCyclingRecordActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditCyclingRecordBinding
+    private lateinit var recordDao: RecordDao
+    private lateinit var distance: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityEditCyclingRecordBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        distance = intent.getStringExtra("Distance") ?: "Cycling"
+
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -23,12 +34,36 @@ class EditCyclingRecordActivity : AppCompatActivity() {
         }
 
         setActionBar()
+
+        recordDao = RecordLogDatabase.getDatabase(this).recordDao()
+
+        binding.saveButton.setOnClickListener {
+            setupSaveButton(distance)
+        }
+    }
+
+    private fun setupSaveButton(distance: String) {
+        val recordType = when (distance) {
+            "Longest Ride" -> RecordType.LongestRide
+            "Biggest Climb" -> RecordType.BiggestClimb
+            "Best Average Speed" -> RecordType.BestAverageSpeed
+            else -> null
+        }
+
+        recordType?.let {
+            lifecycleScope.launch {
+                recordDao.updateRecord(
+                    it,
+                    binding.timeTextField.text.toString(),
+                    getCurrentFormattedDate()
+                )
+            }
+        }
     }
 
     private fun setActionBar() {
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        val distance = intent.getStringExtra("Distance") ?: "Cycling"
         supportActionBar?.title = "$distance record"
     }
 }
--- app/src/main/java/com/example/recordkeeper/EditRunningRecordActivity.kt
+++ app/src/main/java/com/example/recordkeeper/EditRunningRecordActivity.kt
@@ -5,17 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.recordkeeper.databinding.ActivityEditRunningRecordBinding
+import com.example.recordkeeper.roomDataBase.RecordDao
+import com.example.recordkeeper.roomDataBase.RecordLogDatabase
+import com.example.recordkeeper.roomDataBase.RecordType
+import com.example.recordkeeper.roomDataBase.getCurrentFormattedDate
+import kotlinx.coroutines.launch
 
 class EditRunningRecordActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditRunningRecordBinding
+    private lateinit var recordDao: RecordDao
+    private lateinit var distance: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityEditRunningRecordBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        distance = intent.getStringExtra("Distance") ?: "Running"
+
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -23,12 +34,37 @@ class EditRunningRecordActivity : AppCompatActivity() {
         }
 
         setActionBar()
+
+        recordDao = RecordLogDatabase.getDatabase(this).recordDao()
+
+        binding.saveButton.setOnClickListener {
+            setupSaveButton(distance)
+        }
+    }
+
+    private fun setupSaveButton(distance: String) {
+        val recordType = when (distance) {
+            "5km" -> RecordType.FiveKm
+            "10km" -> RecordType.TenKm
+            "Half-Marathon" -> RecordType.HalfMarathon
+            "Marathon" -> RecordType.Marathon
+            else -> null
+        }
+
+        recordType?.let {
+            lifecycleScope.launch {
+                recordDao.updateRecord(
+                    it,
+                    binding.timeTextField.text.toString(),
+                    getCurrentFormattedDate()
+                )
+            }
+        }
     }
 
     private fun setActionBar() {
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        val distance = intent.getStringExtra("Distance") ?: "Running"
         supportActionBar?.title = "$distance record"
     }
 }
